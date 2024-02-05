af6e9cd14cc77d9b4bef8df47250679b12659c87
enriquedcs <enrique.decoss@gmail.com>
2024-02-05 14:36:03 -0600

    first commit Testing

---
+++ cypress/e2e/e2e_tests/test-example.cy.js
@@ -0,0 +1,69 @@
+import config from './config.json'
+import MainPage from '../../page-objects/components/MainPage'
+
+describe('Elements Exist', () => {
+   
+    it('verify Elements exist accross the pages', () =>{
+     
+        cy.visit(`${config.URL2}`)
+        cy.on('window:confirm', cy.stub().as('confirm'))
+        Cypress.on('uncaught:exception', (err, runnable) => {
+            // returning false here prevents Cypress from
+            // failing the test
+        return false
+        })
+
+        //Element Exists
+        cy.get('body')
+            .then($body => {
+                if ($body.find('.flex-md-nowrap .search-category').length) {
+                    return '.flex-md-nowrap .search-category';
+            }
+            return '.dropdown';
+            })
+            .then(selector => {
+            cy.get(selector)
+        })
+
+        //Element is visible
+        cy.get('.flex-md-nowrap .search-category')
+            .then($body => {
+            if ($body.is(':visible')) {
+                cy.get('.flex-md-nowrap .search-category').click()
+            } 
+            else{
+                return
+            }
+        })
+
+        //Improve Method Find Search Button
+        MainPage.elExists('.type-text')    
+    
+
+    })
+
+    it('verify Element is visible accross the pages', () =>{
+     
+        cy.visit(`${config.URL2}`)
+        cy.on('window:confirm', cy.stub().as('confirm'))
+        Cypress.on('uncaught:exception', (err, runnable) => {
+            // returning false here prevents Cypress from
+            // failing the test
+        return false
+        })
+
+        //Element is visible
+        cy.get('.flex-md-nowrap .search-category')
+            .then($body => {
+            if ($body.is(':visible')) {
+                cy.get('.flex-md-nowrap .search-category').click()
+            } 
+            else{
+                return
+            }
+        })
+    
+
+    })
+
+})
---
+++ cypress/page-objects/components/MainPage.js
@@ -0,0 +1,27 @@
+export default class MainPage {
+    static elementOne() {
+        cy.get("[class='col-xs-12 col-md-12 blog-list']:nth-of-type(3)").toMatchImageSnapshot()
+    }
+
+    static elementTwo() {
+        cy.get("[class='col-xs-12 col-md-12 blog-list']:nth-of-type(5)").toMatchImageSnapshot({
+            threshold: 0.1
+        })
+    }
+
+    static search(text){
+        cy.get(`input[name='s']`).type(`${text} {enter}`)
+    }
+
+    static elExists(text){
+        cy.get(text)
+            .then($body => {
+            if ($body.is(':visible')) {
+                cy.get(text).click()
+            } 
+            else{
+                return text
+            }
+        })
+    }
+}
